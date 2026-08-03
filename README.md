db066d4c4fdb435fdeef8f98459dfb64efe1b192
paramendula <paramendula@yandex.ru>
2026-08-03 17:43:25 +0300

    reader struct added, readme small fix

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Version 0.1.0 - work in progress.
 ---
 
 Planned to be:
+
 - Portable
 - Homoiconic (data = code)
 - Lightweight (under 1MiB limit for now)
@@ -18,9 +19,11 @@ Planned to be:
 - Written in Zig (initially, self-written is a goal)
 
 'Quasi-compiled' in context of Lingo means built-in:
+
 - JIT compilation
 - Interpreter and compiler used by the language being accessible with an API (programmable)
 - Lingo instance is a compiler instance
 
 Contribution rules:
+
 - Fully LLM-orchestrated commits/pull requests aren't allowed (AI slop bye-bye).
--- src/read.zig
+++ src/read.zig
@@ -1,39 +1,59 @@
+const std = @import("std");
+
 pub const TokenKind = enum(i32) {
-    // -> '('
+    /// -> '('
     parenOpen,
-    // -> ')'
+    /// -> ')'
     parenClose,
-    // -> '['
+    /// -> '['
     parenOpenSquare,
-    // -> ']'
+    /// -> ']'
     parenCloseSquare,
-    // -> '{'
+    /// -> '{'
     parenOpenCurly,
-    // -> '}'
+    /// -> '}'
     parenCloseCurly,
-    // Standalone or multipart
+    /// Standalone or multipart
     symbol,
-    // Just a string
+    /// Just a string
     string,
-    // Starts with a '#', e.g. '#true'
+    /// Starts with a '#', e.g. '#true'
     special,
-    // Positive/negative whole number
+    /// Positive/negative whole number
     integer,
-    // Floating point
+    /// Floating point
     real,
-    // -> '.' (tail specifier)
+    /// -> '.' (tail specifier)
     dot,
-    // -> ',' (unquote)
+    /// -> ',' (unquote)
     comma,
-    // -> ',*' (unquote-splice)
+    /// -> ',*' (unquote-splice)
     commaStar,
-    // -> '@' (mutability specifier)
+    /// -> '@' (mutability specifier)
     at,
-    // -> ';' (semantic delimiter)
+    /// -> ';' (semantic delimiter)
     semicolon,
-    // -> \n or \r\n
-    // Newlines are important (semantic delimiter)
+    /// -> \n or \r\n
+    /// Newlines are important (semantic delimiter)
     newLine,
 };
 
-pub const Token = union(TokenKind) {};
+pub const Token = union(TokenKind) { symbol: []u8, string: []u8, special: []u8, integer: []u8, real: []u8 };
+
+pub const Options = struct {
+    /// Instead of allocating memory for symbols, string, specials, etc.
+    /// create slices pointing to the source buffer.
+    /// Returned tokens will contain these slices.
+    return_buffer_slices: bool = false,
+};
+
+pub fn Reader(comptime opts: Options) type {
+    _ = opts;
+    return struct {
+        const Self = @This();
+
+        fn next(self: Self) ?Token {
+            _ = self;
+        }
+    };
+}
