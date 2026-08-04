177d0b0c30e64a4189d12ed74c661e82b1e3175b
paramendula <paramendula@yandex.ru>
2026-08-04 15:56:58 +0300

    + some reader impl., small plan for simple-calc example

--- src/read.zig
+++ src/read.zig
@@ -47,13 +47,66 @@ pub const Options = struct {
     return_buffer_slices: bool = false,
 };
 
+pub const InitError = error{
+    /// Returned when the 'return_buffer_slices' flag isn't set, but
+    /// no allocator has been provided
+    AllocatorNotProvided,
+};
+
+pub const ReadError = error{
+    /// Returned when the allocator provided returned a null
+    NotEnoughMemory,
+    /// Returned when the current token being parsed isn't finished yet
+    /// and the byte_reader has no more bytes left (fixable)
+    UnfinishedToken,
+    /// Return when a syntax error has been encountered
+    InvalidToken,
+    /// Returned when the byte_reader has no more bytes left (fixable)
+    EndOfStream,
+};
+
 pub fn Reader(comptime opts: Options) type {
-    _ = opts;
     return struct {
+        byte_reader: std.Io.Reader,
+        allocator: ?std.mem.Allocator,
+        current_kind: ?TokenKind,
+
         const Self = @This();
 
-        fn next(self: Self) ?Token {
+        pub fn init(byte_reader: std.Io.Reader, allocator: ?std.mem.Allocator) InitError!Reader {
+            if (opts.return_buffer_slices) {
+                // TODO: check if fixed
+            } else if (!allocator) {
+                return .AllocatorNotProvided;
+            }
+
+            return Self{
+                .byte_reader = byte_reader,
+                .allocator = allocator,
+            };
+        }
+
+        pub fn deinit(self: *Self) void {
             _ = self;
+        }
+
+        pub fn next(self: *Self) ReadError!Token {
+            const byte = self.byte_reader.takeByte() orelse return .EndOfStream;
+            if (self.current_kind) |k| {
+                _ = k;
+            } else {
+                switch (byte) {
+                    '(' => return .parenOpen,
+                    ')' => return .parenClose,
+                    '[' => return .parenOpenSquare,
+                    ']' => return .parenCloseSquare,
+                    '{' => return .parenOpenCurly,
+                    '}' => return .parenCloseCurly,
+                    '.' => return .dot,
+                    '@' => return .at,
+                    ';' => return .semicolon,
+                }
+            }
         }
     };
 }
